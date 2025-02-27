bc70d7630a768ddbec545b5d0b3c1e391925b096
uuqjz <nils.niehues@kit.edu>
2025-02-27 13:56:43 +0100

    Opacity on hit

--- games/prototype/src/main.cpp
+++ games/prototype/src/main.cpp
@@ -20,6 +20,7 @@
 #include "bn_sprite_items_rocket.h"
 #include "bn_sprite_items_monsters.h"
 #include "bn_sprite_items_head.h"
+#include "bn_blending.h"
 
 namespace
 {
@@ -78,7 +79,7 @@ namespace
     constexpr bn::fixed BOUNCE_FACTOR = 0.75f;
     constexpr bn::fixed MIN_BOUNCE_VELOCITY = 1.0f;
     constexpr int CAMERA_BORDER = 100;
-    constexpr int PLAYER_HIT_POINTS = 3;
+    constexpr int PLAYER_HIT_POINTS = 5;
     constexpr int INVINCIBILITY_FRAMES = 100;
 
     struct Player {
@@ -293,6 +294,9 @@ namespace
 
     void moveEnemiesToPlayer(Player& player, bn::vector<Enemy,MAX_ENEMIES>& enemies, bn::vector<bn::sprite_ptr,PLAYER_HIT_POINTS>& hearts, int& framesSinceLastHit){
         framesSinceLastHit++;
+        if(framesSinceLastHit > INVINCIBILITY_FRAMES){
+            player.sprite.set_blending_enabled(false);
+        }
         for (auto& enemy : enemies){
             bool direction = player.sprite.x() > enemy.sprite.x();
             int steps = SPEED * (direction ? 1 : -1);
@@ -304,6 +308,7 @@ namespace
                 if(hearts.size()>0){
                     hearts.pop_back();
                 }
+                player.sprite.set_blending_enabled(true);
             }
 
             if(isColliding(enemy,player,enemies)){
@@ -321,6 +326,8 @@ int main()
 
     bn::sprite_text_generator text_generator(common::variable_8x16_sprite_font);
     bn::bg_palettes::set_transparent_color(bn::color(16, 16, 16));
+    bn::blending::set_transparency_alpha(0.5);
+
 
     bn::camera_ptr camera = bn::camera_ptr::create(0, 0);
     
